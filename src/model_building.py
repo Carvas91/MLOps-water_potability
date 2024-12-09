3a7dcd68576a55c067d3c94067129dbcc597904c
carvas91 <carvas91@hotmail.com>
2024-12-09 14:50:45 -0500

    new commit

--- src/model_building.py
+++ src/model_building.py
@@ -15,7 +15,7 @@ train_data = pd.read_csv('./data/processed/train_processed.csv')
 X_train = train_data.drop(columns=["Potability"],axis=1)
 y_train = train_data['Potability']
 
-n_estimators = yaml.safe_load(open("./src/params.yaml"))["model_building"]["n_estimators"]
+n_estimators = yaml.safe_load(open("./params.yaml"))["model_building"]["n_estimators"]
 
 clf = RandomForestClassifier(n_estimators=n_estimators)
 clf.fit(X_train, y_train)
