0a8c95b0e177f3268bc03ff644e46e0fbdb568b2
carvas91 <carvas91@hotmail.com>
2024-12-10 13:31:14 -0500

    data collection modularizing of the code

--- src/data_collection.py
+++ src/data_collection.py
@@ -8,31 +8,59 @@ import yaml
 
 
 def load_params(filepath: str)-> float:
-    with open (filepath, 'r') as file:
-        params = yaml.safe_load(file)
-        return params["data_collection"]["test_size"]
+    try:
+        with open (filepath, 'r') as file:
+            params = yaml.safe_load(file)
+            return params["data_collection"]["test_size"]
+    except Exception as e:
+        raise Exception(f"Error loading parameters from {filepath}:{e}")
+
 #test_size = yaml.safe_load(open("./params.yaml"))["data_collection"]["test_size"]
 
 def load_data(filepath: str)-> pd.DataFrame:
-    return pd.read_csv(filepath)
+    try:
+        return pd.read_csv(filepath)
+    except Exception as e:
+        raise Exception(f'Error loading data from {filepath}: {e}')
 #data = pd.read_csv('./water_potability.csv')
 
 
 def split_data(data: pd.DataFrame, test_size: float)-> tuple[pd.DataFrame,pd.DataFrame]:
-    return train_test_split(data, test_size=test_size, random_state=41)
+    try:
+        return train_test_split(data, test_size=test_size, random_state=41)
+    except ValueError as e:
+        raise ValueError(f'Error splitting data: {e}')
 #train_data, test_data = train_test_split(data, test_size=test_size, random_state=41)
 
 def save_data(df: pd.DataFrame, filepath: str)-> None:
-    df.to_csv(filepath, index=False)
+    try:
+        df.to_csv(filepath, index=False)
+    except Exception as e:
+        raise Exception(f'Erro saving data to {filepath}: {e}')
 
 def main():
-    data_filepath = './water_potability.csv'
-    params_filepath = './params.yaml'
-    raw_data_path =  os.path.join("data","raw")
+    try:
+        data_filepath = './water_potability.csv'
+        params_filepath = './params.yaml'
+        raw_data_path =  os.path.join("data","raw")
+
+        data = load_data(data_filepath)
+        test_size = load_params(params_filepath)
+        train_data, test_data = split_data(data, test_size=test_size)
+        os.makedirs(raw_data_path)
+        save_data(train_data, os.path.join(raw_data_path, 'train.csv'))
+        save_data(test_data, os.path.join(raw_data_path, 'test.csv'))
+    except Exception as e:
+        raise Exception(f"An error ocurred {e}")
+
+
+
+if __name__ == "__main__":
+    main()
 
 #data_path = os.path.join("data","raw")
 
-os.makedirs(data_path)
+#os.makedirs(data_path)
 
-train_data.to_csv(os.path.join(data_path,"train.csv"),index = False)
-test_data.to_csv(os.path.join(data_path,"test.csv"),index=False)
+#train_data.to_csv(os.path.join(data_path,"train.csv"),index = False)
+#test_data.to_csv(os.path.join(data_path,"test.csv"),index=False)
